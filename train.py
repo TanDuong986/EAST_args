bae793f0142b01b18583c3a817251e87c54d0c9a
dtan <duongtanrb@gmail.com>
2023-05-07 14:39:39 +0700

    fix error weight

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ def opt_parse():
 	parser.add_argument("--train-img-fol",type=str,required=True,help="path of folder image train")
 	parser.add_argument("--train-lb-fol",type=str,required=True,help="path of label folder")
 	parser.add_argument("--save-fol",type=str,required=True,help="path of output weight")
-	parser.add_argument("--weight-pt",str=str,default='no_signal',help="optional about input pre-trained, none is train from scratch")
+	parser.add_argument("--weight-pt",type=str,default='no_signal',help="optional about input pre-trained, none is train from scratch")
 	parser.add_argument("--batch",type=int,default=16,help="batch size of train data")
 	parser.add_argument("--lr",type=float,default=1e-3,help="start learning rate")
 	parser.add_argument("--epoch",type=int,default=10,help="epoch train")
